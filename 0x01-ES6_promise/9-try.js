3eda3caecb214fbd177301d0a584426196f884e2
Wonderful Emmanuel Adebayo <adebayowondeful@gmail.com>
2023-10-14 03:29:48 +0100

    ES6 Promises

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
