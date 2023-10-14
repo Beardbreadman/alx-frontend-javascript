f7f9f3199250b6694eb98c91e5b038f2aea6c3f4
beardbreadman <adebayowondeful@gmail.com>
2023-10-14 02:11:35 +0000

    javascript

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+export function taskFirst() {
+	// \n
+	const task = 'I make use of const when i cam.';
+	return task;
+}// \n
+
+export function getlist() {
+	 return ' is okay';
+ }
+
+export function taskNext() {
+	let conbination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
